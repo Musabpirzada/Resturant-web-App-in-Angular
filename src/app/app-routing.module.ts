7c5ee833fc07a1e7d4c960641f9c67f16a8bfc50
Musab <musabpirzada1@gmail.com>
2022-09-17 16:17:18 +0500

    Routing added part 2 in menu, home and contact ..ABout is remaining

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common'
 import { RouterModule, Routes } from '@angular/router';
 import { routes } from './routes';
-// const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes),
