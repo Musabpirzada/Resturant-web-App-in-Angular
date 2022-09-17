feecd0ccf06febf725ab9457d3eaec06d43ec5c4
Musab <musabpirzada1@gmail.com>
2022-09-17 08:30:32 +0500

    Routing added and Functional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { routes } from './routes';
+// const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+  CommonModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
