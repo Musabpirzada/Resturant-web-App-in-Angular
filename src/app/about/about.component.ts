0257f8eeb0f47f538fcc321775254e93b473bdd7
Musab <musabpirzada1@gmail.com>
2022-09-18 10:06:43 +0500

    Assignmnet 2 task 2 completed

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LeaderService } from '../services/leader.service';
+import { Leader} from '../shared/leader';
+import { LEADERS } from '../shared/leaders';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+Leaders : Leader[] = LEADERS;
+  constructor(private leaderservice: LeaderService) { }
 
   ngOnInit(): void {
+    this.Leaders = this.leaderservice.getLeaders();
   }
 
 }
